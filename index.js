b83925031d051e848d48b6cae61a3f75781a0e5e
Will Drougas <wdrougas@gmail.com>
2019-08-12 21:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string = string.toLowerCase()) {
+  if (string === string.toLowerCase()) {
     return "I can\'t hear you!"}
     
-  if (string + string.toUpperCase()) {
+  if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
 }
